5b6c42d46d8b694df853d36e75d9164798830c34
samvalvi <samuelvalerin@gmail.com>
2021-03-02 18:07:43 +0000

    exercise 3

--- exercises/02.2-maping-array-of-objects-to-li/index.js
+++ exercises/02.2-maping-array-of-objects-to-li/index.js
@@ -7,8 +7,15 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
+const mappingFunction = (item, i) => {
+	return (
+		<li key={i} className="list-group-item">
+			{item}
+		</li>
+	);
+};
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = (<ul className="list-group m-5"></ul>);
+const content = <ul className="list-group m-5">{planets.map(mappingFunction)}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
