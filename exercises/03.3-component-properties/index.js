0d9ee04dce25e7a1b2b27a5be83d467824cb8818
samvalvi <samuelvalerin@gmail.com>
2021-03-02 19:17:15 +0000

    exercise 3.5

--- exercises/03.3-component-properties/index.js
+++ exercises/03.3-component-properties/index.js
@@ -6,21 +6,34 @@ const BootstrapCard = props => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/" alt="Card image cap" />
+			<img className="card-img-top" src={props.imageUrl} alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
-				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
-					Go to wikipedia
+				<p className="card-text">{props.description}</p>
+				<a href={props.buttonUrl} className="btn btn-primary">
+					{props.buttonLabel}
 				</a>
 			</div>
 		</div>
 	);
 };
 BootstrapCard.propTypes = {
-	title: PropType.string
-		// 2) add here the new properties into the proptypes object
+	title: PropType.string,
+	// 2) add here the new properties into the proptypes object
+	imageUrl: PropType.string,
+	description: PropType.string,
+	buttonUrl: PropType.string,
+	buttonLabel: PropType.string
 };
 
-
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
+ReactDOM.render(
+	<BootstrapCard
+		title="Paul Mccartney"
+		imageUrl="https://upload.wikimedia.org/wikipedia/commons/d/d6/Paul_McCartney_in_October_2018.jpg"
+		description=" is an English singer, songwriter, musician, and record and film producer who gained worldwide fame as co-lead vocalist and bassist for the Beatles."
+		buttonUrl="https://en.wikipedia.org/wiki/Paul_McCartney"
+		buttonLabel="Go to Wikipedia"
+	/>,
+	document.querySelector("#myDiv")
+);
--- exercises/03.4-jumbotron/index.js
+++ exercises/03.4-jumbotron/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import PropType from "prop-types";
 
 const Jumbotron = props => {
-	//here you have to return expected html using the properties being passed to the component
+    //here you have to return expected html using the properties being passed to the component
+    return (
+	<div className="jumbotron m-5">
+		<h1 className="display-4">{props.title}</h1>
+		<p className="lead">{props.description}</p>
+		<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
+			{props.buttonLabel}
+		</a>
+	</div>
+    )
 };
 
 Jumbotron.propTypes = {
 	//proptypes here
+	title: PropType.string,
+	description: PropType.string,
+	buttonLabel: PropType.string,
+	buttonURL: PropType.string
 };
 
 ReactDOM.render(
